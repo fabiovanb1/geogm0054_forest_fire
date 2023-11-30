953eac9970b85955fcd23caad5f4e591bcdf0c01
Fabio Vanegas <fabiovanb@hotmail.com>
2023-11-30 20:56:27 +0000

    other changes

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -9,10 +9,10 @@ from ca_fire import evolve_forest, write_netcdf
 
 
 # defaults
-GRID_N_X = 200  # sub-divisions along X axis
-PROB_GROWTH = 1.0e-4  # probability of new growth per cell per unit time
-PROB_NEW_FIRE = 1.0e-6  # probability of new fire per cell per unit time
-N_TIME_STEP = 200000  # number of time-steps
+GRID_N_X = 300  # sub-divisions along X axis
+PROB_GROWTH = 1.0e-5  # probability of new growth per cell per unit time
+PROB_NEW_FIRE = 1.0e-7  # probability of new fire per cell per unit time
+N_TIME_STEP = 100000  # number of time-steps
 VERBOSE = True # set to False to suppress printing
 
 # run the model
